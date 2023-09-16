977864ecb6ffd6eb6f90e5f7833004d744d1ddc9
shehap <shehap@gmail.com>
2023-09-16 11:34:13 +0300

    breaking news

--- src/main/java/com/model/Roles.java
+++ src/main/java/com/model/Roles.java
@@ -1,5 +1,11 @@
 package com.model;
 
+
+/*
+*  actually, USER is the only role that is used in this project yet.
+* ADMIN and SUPERMAN can be used in the future.
+* */
+
 public enum Roles {
     USER,
     ADMIN,
